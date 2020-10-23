9c9bce51d2632f64daae4032d6da70029f88a206
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-10-22 18:45:06 -0700

    working

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const fs = require("fs")
 const express = require ("express");
 const path = require("path");
-
+let notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
-app.use("/assets", express.static("public/assets"));
+app.use(express.static("public"));
 
-let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 app.get('/notes', (req, res) => {
     //gets notes.html and displays on page
     res.sendFile(path.join(__dirname + '/public/notes.html'));
@@ -32,11 +31,11 @@ app.post("/api/notes", (req, res) => {
     if (notes.length ===0) {
         newInput.id = 1;
     } else {
-        // increase id number by 1 each time theres a new note
+        // increase id every time note is added
         const newNoteId= notes[notes.length -1].id + 1
         newInput.id = newNoteId;
     }
-    // see if newInput working properly
+    
     console.log(newInput);
     notes.push(newInput);
     console.log(notes);
@@ -45,16 +44,19 @@ app.post("/api/notes", (req, res) => {
     res.json(newInput);
 });
 
-app.delete("/api/notes/:id", (req,res) =>{
-    let id = JSON.parse(fs.readFileSync(path.join(__dirname,"/db/db.json")));
-    let newNote = notes.filter((note) =>{
-        return notes.id !== id;
+app.delete('/api/notes/:id', function(req,res){
+    const id= parseInt(req.params.id);
+    console.log(id);
+    // filtering through for chosen id
+    const newInputs= notes.filter((note) => {
+        // only return the other notes that are not = to the chosen id
+        return note.id !== id;
     });
-    fs.writeFileSync(path.join(__dirname + "/db/db.json")), JSON.stringify(newNote);
-    notes = newNote;
-    res.json(newNote);
+    // rewrite the db.json file with the new inputs, so this should not include the deleted note
+    fs.writeFileSync("./db/db.json", JSON.stringify(newInputs));
+    notes = newInputs;
+    res.json(newInputs);
 });
-
 app.listen(PORT, function(){
     console.log("Listening on PORT:" + PORT);
 });
